d5a501557bc44583dca786fa92da51a85eb9b367
SyedMuhammadRehan <syedrehan55.rs@gmail.com>
2022-09-27 20:58:50 +0500

    count example

---
+++ lib/screens/count_example.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CountExapmle extends StatefulWidget {
+  const CountExapmle({super.key});
+
+  @override
+  State<CountExapmle> createState() => _CountExapmleState();
+}
+
+class _CountExapmleState extends State<CountExapmle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
