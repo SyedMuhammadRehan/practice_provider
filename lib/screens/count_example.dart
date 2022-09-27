5488345d107e98f2ada21adecf9a009316de3190
SyedMuhammadRehan <syedrehan55.rs@gmail.com>
2022-09-27 23:23:49 +0500

    update count example

--- lib/screens/count_example.dart
+++ lib/screens/count_example.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:practice_provider/provider/count_provider.dart';
+import 'package:provider/provider.dart';
 
 class CountExapmle extends StatefulWidget {
   const CountExapmle({super.key});
@@ -11,6 +12,21 @@ class CountExapmle extends StatefulWidget {
 class _CountExapmleState extends State<CountExapmle> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final countProvider = Provider.of<CountProvider>(context, listen: false);
+    print(countProvider.count.toString());
+
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: const Text("Counter")),
+      body: Center(
+        child: Consumer<CountProvider>(builder: ((context, value, child) {
+          return Text(value.count.toString());
+        })),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (() {
+            countProvider.setcount();
+          }),
+          child: const Icon(Icons.add)),
+    );
   }
 }
